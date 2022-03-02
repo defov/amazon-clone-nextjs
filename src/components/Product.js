b7aab47268ca8f57821f0ae6782f5c3569b20c26
defov <denivarna1512@gmail.com>
2022-03-02 17:01:58 +0200

    product feed

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,54 +1,57 @@
-import Image from 'next/image'
-import { useState } from 'react'
-import { StarIcon } from '@heroicons/react/solid'
-import NumberFormat from 'react-number-format';
+import Image from "next/image"
+import { useState } from "react"
+import { StarIcon } from "@heroicons/react/solid"
+import NumberFormat from "react-number-format"
 
-const MAX_RATING = 5;
-const MIN_RATING = 1;
+const MAX_RATING = 5
+const MIN_RATING = 1
 
 const Product = ({ id, title, price, description, category, image }) => {
-    const [rating] = useState(
-        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
-    )
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  )
 
-    const [hasPrime] = useState(Math.random() < 0.5);
-  
-    return (
-        <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-            <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
+  const [hasPrime] = useState(Math.random() < 0.5)
 
-            <Image
-                src={image} 
-                height={200}
-                width={200}
-                objectFit="contain"
-            />
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
 
-            <h4 className='my-3'>{title}</h4>
+      <Image src={image} height={200} width={200} objectFit="contain" />
 
-            <div className='flex'>
-                {Array(rating)
-                  .fill().map((_, i) => (
-                    <StarIcon className='h-5 text-yellow-500' />
-                ))}
-            </div>              
+      <h4 className="my-3">{title}</h4>
 
-            <p className='text-xs my-2 line-clamp-2'>{description}</p>
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
+      </div>
 
-            <div className='mb-5'>
-                <NumberFormat value={price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-            </div>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-            {hasPrime && (
-                <div className='flex items-center space-x-2 -mt-5'>
-                    <img className="w-12" src="/images/prime_tag.png" alt="" />
-                    <p className='text-xs text-gray-500'>FREE Next-day Delivery</p>
-                </div>
-            )}
+      <div className="mb-5">
+        <NumberFormat
+          value={price}
+          displayType={"text"}
+          thousandSeparator={true}
+          prefix={"$"}
+        />
+      </div>
 
-            <button className='mt-auto button'>Add to basket</button>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="/images/prime_tag.png" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
-    )
+      )}
+
+      <button className="mt-auto button">Add to basket</button>
+    </div>
+  )
 }
 
 export default Product
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,22 +1,40 @@
 import Product from "./Product"
 
 const ProductFeed = ({ products }) => {
-    return (
-        <div>
-            <div>ProductFeed</div>
-            {products && products.map(({ id, title, price, description, category, image }) => (
-                <Product
-                    key={id}
-                    id={id}
-                    title={title}
-                    price={price}
-                    description={description}
-                    category={category}
-                    image={image}
-                />
-            ))}
-        </div>
-    )
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            {...{ id, title, price, description, category, image }}
+          />
+        ))}
+
+      <img className="mx-auto md:col-span-full" src="/images/ad.jpg" alt="" />
+
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              {...{ id, title, price, description, category, image }}
+            />
+          ))}
+      </div>
+
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            {...{ id, title, price, description, category, image }}
+          />
+        ))}
+    </div>
+  )
 }
 
 export default ProductFeed
